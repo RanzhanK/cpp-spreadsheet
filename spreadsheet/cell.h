0517319c07f7e3d9beaf610b588adbbb107c0f0c
RanzhanK <ranzhan@yandex.ru>
2023-07-30 15:34:45 +0300

    end 16 sprint project

--- spreadsheet/cell.h
+++ spreadsheet/cell.h
@@ -5,6 +5,8 @@
 
 #include <functional>
 #include <unordered_set>
+#include <stack>
+#include <set>
 
 class Sheet;
 
@@ -21,16 +23,61 @@ public:
     std::vector<Position> GetReferencedCells() const override;
 
     bool IsReferenced() const;
+    void InvalidateAllCache(bool flag);
 
 private:
-    class Impl;
-    class EmptyImpl;
-    class TextImpl;
-    class FormulaImpl;
+
+    class Impl {
+    public:
+        virtual Value GetValue() const = 0;
+        virtual std::string GetText() const = 0;
+        virtual std::vector<Position> GetReferencedCells() const;
+
+        virtual bool HasCache();
+        virtual void InvalidateCache();
+
+        virtual ~Impl() = default;
+    };
+
+    class EmptyImpl : public Impl {
+    public:
+
+        Value GetValue() const override;
+        std::string GetText() const override;
+    };
+
+    class TextImpl : public Impl {
+    public:
+
+        explicit TextImpl(std::string text);
+        Value GetValue() const override;
+        std::string GetText() const override;
+
+    private:
+        std::string text_;
+    };
+
+    class FormulaImpl : public Impl {
+    public:
+
+        explicit FormulaImpl(std::string text, SheetInterface& sheet);
+
+        Value GetValue() const override;
+        std::string GetText() const override;
+        std::vector<Position> GetReferencedCells() const override;
+
+        bool HasCache() override;
+        void InvalidateCache() override;
+
+    private:
+        mutable std::optional<FormulaInterface::Value> cache_;
+        std::unique_ptr<FormulaInterface> formula_ptr_;
+        SheetInterface& sheet_;
+    };
 
     std::unique_ptr<Impl> impl_;
+    Sheet& sheet_;
 
-    // Добавьте поля и методы для связи с таблицей, проверки циклических 
-    // зависимостей, графа зависимостей и т. д.
-
+    std::set<Cell*> dependent_cells_;
+    std::set<Cell*> referenced_cells_;
 };
--- spreadsheet/formula.cpp
+++ spreadsheet/formula.cpp
@@ -1,1 +1,104 @@
-/* разместите здесь свой код */
+#include "formula.h"
+
+#include "FormulaAST.h"
+
+#include <algorithm>
+#include <cassert>
+#include <cctype>
+#include <sstream>
+
+using namespace std::literals;
+
+std::ostream& operator<<(std::ostream& output, FormulaError fe) {
+    return output << "#DIV/0!";
+}
+
+namespace {
+    class Formula : public FormulaInterface {
+    public:
+
+        explicit Formula(std::string expression) try : ast_(ParseFormulaAST(expression)) {}
+        catch (...) {
+            throw FormulaException("formula is syntactically incorrect");
+        }
+
+        Value Evaluate(const SheetInterface& sheet) const override {
+
+            try {
+
+                std::function<double(Position)> args = [&sheet](const Position pos)->double {
+
+                    if (pos.IsValid()) {
+
+                        const auto* cell = sheet.GetCell(pos);
+                        if (cell) {
+
+                            if (std::holds_alternative<double>(cell->GetValue())) {
+                                return std::get<double>(cell->GetValue());
+
+                            } else if (std::holds_alternative<std::string>(cell->GetValue())) {
+
+                                auto str_value = std::get<std::string>(cell->GetValue());
+                                if (str_value != "") {
+                                    std::istringstream input(str_value);
+                                    double num = 0.0;
+
+                                    if (input.eof() && input >> num) {
+                                        return num;
+                                    } else {
+                                        throw FormulaError(FormulaError::Category::Value);
+                                    }
+
+                                } else {
+                                    return 0.0;
+                                }
+
+                            } else {
+                                throw FormulaError(std::get<FormulaError>(cell->GetValue()));
+                            }
+
+                        } else {
+                            return 0.0;
+                        }
+
+                    } else {
+                        throw FormulaError(FormulaError::Category::Ref);
+                    }
+                };
+
+                return ast_.Execute(args);
+
+            } catch (const FormulaError& evaluate_error) {
+                return evaluate_error;
+            }
+        }
+
+        std::string GetExpression() const override {
+            std::ostringstream out;
+            ast_.PrintFormula(out);
+
+            return out.str();
+        }
+
+        std::vector<Position> GetReferencedCells() const override {
+            std::vector<Position> cells;
+            for (const auto& cell : ast_.GetCells()) {
+
+                if (cell.IsValid()) {
+                    cells.push_back(cell);
+                } else {
+                    continue;
+                }
+            }
+            return cells;
+        }
+
+    private:
+        FormulaAST ast_;
+    };
+
+}//end namespace
+
+std::unique_ptr<FormulaInterface> ParseFormula(std::string expression) {
+    return std::make_unique<Formula>(std::move(expression));
+}
